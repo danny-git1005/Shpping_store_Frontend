c6f730d75425455a1dcaa3dabbd32b89fed8f954
danny-git1005 <danny871005@gmail.com>
2023-12-12 17:26:57 +0800

    finish get product info api

--- api/getProductInfo.js
+++ api/getProductInfo.js
@@ -10,16 +10,7 @@ function get_product_info(name,web){
             console.log("success");
             //console.log(response);
  
-            picture = document.querySelector("img");
-            movie_name = document.getElementsByClassName("left_row_title");
-            release = document.getElementsByClassName("right_row_desc");
-
-            movie_name[0].innerText = response['movie_name'];
-            movie_name[0].setAttribute("id",response['movieid']);
-            picture.src = response['movie_img_href'];
-            release[0].innerText = response['movie_release'];
-            release[1].innerText = response['runtime'];
-            release[2].innerText = response['director'];
+            
             if ( web === "detail")
             {
                 release[3].innerText = response['story'];
