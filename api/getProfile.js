8bc72e39c809e9f2b02c0418bcf6199cb81ea3be
danny-git1005 <danny871005@gmail.com>
2023-12-13 20:29:17 +0800

    finish 使用者資訊頁面

--- api/getProfile.js
+++ api/getProfile.js
@@ -9,16 +9,16 @@ function get_profile(user){
         
         success: function(response){  
 
-            console.log(response);
             const name = response['name'].split(" ");
             let fisrt_name = name[0];
             let last_name = name[1];
             let address = response['address'];
 
-            console.log($('#fname'));
             $('#fname')[0].value   = fisrt_name;
             $('#lname')[0].value   = last_name;
             $('#address')[0].value = address;
+            $('#accound_id')[0].textContent = user;
+            
 
         },
         error: function( XMLHttpRequest,response){
