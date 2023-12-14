679327960c749ba6727a296cc76ffd50218eeaf3
danny-git1005 <danny871005@gmail.com>
2023-12-14 15:56:26 +0800

    finish shop頁面 種類尋找

--- api/getShop.js
+++ api/getShop.js
@@ -1,19 +1,5 @@
 $(document).ready(function(){
 
-    getShop('all');
-
-    $('.product').on('click', function() {
-        let name = $(this).children('.product-detail').children()[1].innerText;
-        console.log(name);
-        url = "./product.html?productName="+name;
-        window.location.href = url;
-    })
-
-
-    $('#cat desktop').click(function() { 
-        getShop('desktop'); 
-    });
-    
     function getShop(category){
         console.log(category);
         $.ajax({
@@ -23,28 +9,22 @@ $(document).ready(function(){
             data:{'submit':'shop','category':category},
             
             success: function(response){  
-                
+                console.log(response);
                 let all_product = $(".product");
                 for (var i=0; i <= all_product.length; i++){
-                    
-                    if (response[i]['category'] == 'desktop' ){
+                    let cate = response[i]['category'];
+                    if (cate == "desktop" ){
                         all_product[i].children[0].children[0].src = "./img/desktop_picture/000001_1689664331.jpg";
                     }
-                    else if ( response[i]['category'] == 'laptop' ){
+                    else if ( cate == 'laptop' ){
                         all_product[i].children[0].children[0].src = "./img/laptop_picture/l000002_1702017530.jpg";
                     }
                     else{
                         all_product[i].children[0].children[0].src = "./img/monitor_picture/l000001_1702226140.jpg";
                     }
-                    
-                   
-                
                     all_product[i].children[1].children[0].textContent = response[i]['category'];
                     all_product[i].children[1].children[1].textContent = response[i]['product name'];
                     all_product[i].children[1].children[2].textContent = response[i]['price'] + " NTD";
-                    
-
-    
                 }
                 
             },
@@ -55,6 +35,29 @@ $(document).ready(function(){
             }
         });
 
-     }
+    }
+    getShop('all');
+
+    $('.product').on('click', function() {
+        let name = $(this).children('.product-detail').children()[1].innerText;
+        console.log(name);
+        url = "./product.html?productName="+name;
+        window.location.href = url;
+    })
+
+
+    $('#cat_desktop').on('click', function() { 
+        getShop('desktop'); 
+    });
+
+    $('#cat_laptop').on('click', function() { 
+        console.log("in");
+        getShop('laptop'); 
+    });
+
+    $('#cat_monitor').on('click', function() { 
+        getShop('monitor'); 
+    });
+    
 
 });
